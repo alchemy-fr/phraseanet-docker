597538ab06492a2d9b8d130973eb90aca5f6a81c
nmaillat <maillat@alchemy.fr>
2018-12-21 18:01:10 +0100

    add git, supervisor by apt and set bin/setup system install

--- docker-compose/sandbox/phraseanet-dev-nmaillat/docker/php/docker-entrypoint.sh
+++ docker-compose/sandbox/phraseanet-dev-nmaillat/docker/php/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ ! -f "/var/Phraseanet/Phraseanet/config/configuration.yml" ]
 then
     cd /var/Phraseanet/Phraseanet/
     make
-    bin/setup system:install --db-host=db --db-user=root --db-password=root --email=admin@alchemy-dockerdev.fr --password=admin --appbox=ab_master --databox=db_databox1 --quiet
+    bin/setup system:install --db-host=db --db-user=root --db-password=root --email=admin@alchemy-dockerdev.fr --password=admin --appbox=ab_master --databox=db_databox1  --db-template=Dublincore  --es-host=elasticsearch --quiet
 	#curl -o /tmp/Phraseanet.zip https://www.phraseanet.com/builds/alchemy-fr-Phraseanet-v4.0.3.zip
 	#unzip /tmp/Phraseanet.zip -d /var/ 2> /dev/null || true
 	#cp /var/configuration-compiled.php /var/Phraseanet/config/configuration-compiled.php
