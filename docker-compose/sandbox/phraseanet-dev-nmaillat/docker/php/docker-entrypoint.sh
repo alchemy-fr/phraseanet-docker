aacfc5c738659ff5333772b865600ae72234c1e0
nmaillat <maillat@alchemy.fr>
2018-12-23 23:49:24 +0100

    make  work in 4.1 not with 4.0  in nic sanbox, to do: elasticsearch index , debugger config

--- docker-compose/sandbox/phraseanet-dev-nmaillat/docker/php/docker-entrypoint.sh
+++ docker-compose/sandbox/phraseanet-dev-nmaillat/docker/php/docker-entrypoint.sh
@@ -2,17 +2,44 @@
 
 set -e
 
+
+if [ ! -e "/var/Phraseanet/Phraseanet/vendor" ]
+then
+    chown www-data: -R /var/www
+    
+    #### install Phraseanet dependecies 
+    su www-data -s /bin/sh
+    export HOME=/var/wwww/
+    #env | grep HOME > /var/www/test.txt
+    #env | grep HOME > /var/Phraseanet/test.txt
+    cd /var/Phraseanet/Phraseanet
+    make
+fi
+
 if [ ! -f "/var/Phraseanet/Phraseanet/config/configuration.yml" ]
 then
-    cd /var/Phraseanet/Phraseanet/
-    make
-    bin/setup system:install --db-host=db --db-user=root --db-password=root --email=admin@alchemy-dockerdev.fr --password=admin --appbox=ab_master --databox=db_databox1  --db-template=Dublincore  --es-host=elasticsearch --quiet
+cd /var/Phraseanet/Phraseanet/
+   
+    
+
+    #### Phraseanet install 
+    bin/setup system:install --email=admin@phrdocker.dev --password=admin --db-host=db --db-port=3306 --db-user=root --db-password=root --db-template=en-simple --appbox=ab_master --databox=db_databox1 --server-name=Alchemy-dockerdev.dck --data-path=/var/Phraseanet/Phrasea_datas -y
+    
+
+    #### download and install package version
+    
 	#curl -o /tmp/Phraseanet.zip https://www.phraseanet.com/builds/alchemy-fr-Phraseanet-v4.0.3.zip
 	#unzip /tmp/Phraseanet.zip -d /var/ 2> /dev/null || true
+	#bin/setup system:install --email=admin@phrdocker.dev --password=admin --db-host=db --db-port=3306 --db-user=root --db-password=root --db-template=en-simple --appbox=ab_master --databox=db_databox1 --server-name=Alchemy-dockerdev.dck --data-path=/var/Phraseanet/Phrasea_datas -y
+    
+    # with dataset for Application Box and databox 
 	#cp /var/configuration-compiled.php /var/Phraseanet/config/configuration-compiled.php
     #cp /var/configuration.yml /var/Phraseanet/config/configuration.yml
     #cp /var/proxies/* /var/Phraseanet/resources/proxies/
     
+    # Create ElasticSearch indexes
+    # bin/console s:i:c
+
 fi	
 
 
@@ -22,5 +49,3 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 exec "$@"
-
-
