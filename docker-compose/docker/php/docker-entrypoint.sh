31b4a396a6f1c7f77fabbf6bfe4d6cd13af5e4f2
nmaillat <maillat@alchemy.fr>
2019-03-15 19:06:59 +0100

    elasticsearch setting in configuration.yml

--- docker-compose/docker/php/docker-entrypoint.sh
+++ docker-compose/docker/php/docker-entrypoint.sh
@@ -26,7 +26,20 @@ then
     #### Phraseanet install 
     bin/setup system:install --email=admin@phrdocker.dev --password=admin --db-host=db --db-port=3306 --db-user=root --db-password=root --db-template=en-simple --appbox=ab_master --databox=db_databox1 --server-name=Alchemy-dockerdev.dck --data-path=/var/Phraseanet/Phrasea_datas -y
     
+    ## change elasticsearch server host and create index
+   
+    bin/setup system:config set main.search-engine.options.host elasticsearch
+    bin/setup system:config set main.search-engine.options.minScore 2
+    bin/setup system:config set main.search-engine.options.base_aggregate_limit 10
+    bin/setup system:config set main.search-engine.options.collection_aggregate_limit 10
+    bin/setup system:config set main.search-engine.options.doctype_aggregate_limit 10
+    bin/console compile:configuration
+    bin/console searchengine:index -c
 
+    ## enable API and disable ssl on it
+    bin/setup system:config set registry.api-clients.api-enabled true
+    bin/setup system:config set main.api_require_ssl false
+    bin/console comp:conf
 
 
 fi	
